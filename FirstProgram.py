8364f794f8ed816cf3af6e835ff02a4dcef47185
CACerny <cadencerny@unomaha.edu>
2024-08-28 16:42:45 +0000

    First update

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,25 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Caden Cerny
+#Date: 8/28/2024
+#Assignment: Lab 1
+#Purpose: To ask a user for their name, and calculate their birth year.
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  name = input("What is your name: ")
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("How old are you: ")
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
+  age = int(age)
+  birth_year = 2024 - age - 1
+  print("You were born in", birth_year)
+
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,14 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Caden Cerny
+#Date: 8/28/2024
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  name = input()
   #Print the story with the user supplied words.
-
+  print("Hello my name is spaceman " + name1 + ". I am on my way to planet " + name2 + ". I will be gone for " + number1 + " days. I am very " + adjective1 + " about the trip but I will miss my " + noun1 + ". I have heard that the atmosphere there is " + adjective2 + ". Luckily my pet " + animal1 + " packed me a jacket to keep me " + adjective3 + ". When I land on the planet I will " + verb1 + " for joy. I am " + adjective4 + " to walk on another planet. I could not be more ")
 
 
 #Call the main function if this is the file being run.
